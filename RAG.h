ad9fd0a1a9260ef1445a1b7056b9927da09f6bb5
Tooky76 <34076488+Tooky76@users.noreply.github.com>
2017-12-12 20:20:20 +0100

    Create RAG.h
    Fichier header de RAG.c.

---
+++ RAG.h
@@ -0,0 +1,20 @@
+#ifndef RAG_H
+#define RAG_H
+
+#include "func.h"
+#include <stdlin.h>
+#include <stdio.h>
+
+typedef struct RAG * rag;
+
+extern rag create_RAG(image, int,int);
+
+static void init_moments_priv(rag,int,int);
+
+static void init_father_priv(rag);
+
+static void init_neighbors_priv(rag,int,int);
+
+static void init_partition_error_priv(rag);
+
+#endif
