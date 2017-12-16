c64a67502f151be41e8157d3924061d20d0786ce
Tooky76 <34076488+Tooky76@users.noreply.github.com>
2017-12-16 13:59:38 +0100

    Update func.c
    Ajout des commentaires et changements de M1,M2 et M3 en M0,M1 et M2 conforme avec les notations de l'énoncé

--- func.c
+++ func.c
@@ -1,9 +1,20 @@
 #include "func.h"
 
+/**
+ * Calcul les moments d'ordre 0 à 2 du bloc donné en second paramètre
+ *
+ * @param self Image à traiter
+ * @param block_id Indice du block à traiter dans l'image donnée en premier paramètre
+ * @param n Nombre de blocs sur la largeur
+ * @param m Nombre de blocs sur la hauteur
+ * @param M0 Pointeur sur le moment d'ordre 0 du block correspondant
+ * @param M1 Pointeur sur le moment d'ordre 1 du block correspondant
+ * @param M2 Pointeur sur le moment d'ordre 2 du block correspondant
+ *
+ */
+extern void give_moments(image self, int block_id, int n, int m, int *M0, double *M1, double *M2 ) {
 
-extern void give_moments(image self, int block_id, int n, int m, int *M1, double *M2, double *M3 ) {
-
-    Point coord; /* Top left block coord */
+    Point coord; /* Coordonnées du coin en haut à gauche */
 
     int i, j, d;
 
@@ -12,16 +23,18 @@ extern void give_moments(image self, int block_id, int n, int m, int *M1, double
 
     /* Initialisation des moments */
 
-    *M1 = 0;
+    *M0 = 0;
     for ( i=0 ; i<3 ; i++ ) {
+        M1[i] = 0.0;
         M2[i] = 0.0;
-        M3[i] = 0.0;
     }
 
     /* Fin de l'initialisation */
 
-    *M1 = block_width * block_heigth; /* Valeur du moment d'ordre 1 */
+    /* Valeur du moment d'ordre 1 */
+    *M0 = block_width * block_heigth;
 
+    /* Valeur des moments d'ordre 1 et 2 */
     COORDX(coord) = (block_id / m) * block_width;
     COORDY(coord) = ((block_id % m) -1) * block_heigth;
 
@@ -29,8 +42,8 @@ extern void give_moments(image self, int block_id, int n, int m, int *M1, double
         for ( i=0 ; i<block_heigth ; i++ ) {
             for ( j=0 ; j<block_width ; j++ ) {
 
-                *M2[d] += image_get_comp(self, coord, d);
-                *M3[d] += (*M2[d])*(*M2[d]);
+                *M1[d] += image_get_comp(self, coord, d);
+                *M2[d] += (*M2[d])*(*M2[d]);
 
                 COORDX(coord)++;
             }
